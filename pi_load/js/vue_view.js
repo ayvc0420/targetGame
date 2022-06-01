03403caa6329041164546b1de0bfc324f365368f
ayvc0420 <ayvc0420@gmail.com>
2022-06-01 19:16:04 +0800

    10 html

--- pi_load/js/vue_view.js
+++ pi_load/js/vue_view.js
@@ -178,7 +178,7 @@
         computed:{
             targetNumber(){
                 const min = 1;
-                const max = 10;
+                const max = 9;
                 return Math.floor(Math.random() * (max - min +1) + min);
             },
             viewPosition(){

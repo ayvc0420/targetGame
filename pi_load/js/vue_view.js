c3cb363eeed50eab914d93debb3559aebf53ea93
ayvc0420 <ayvc0420@gmail.com>
2022-06-01 19:20:31 +0800

    kamera

--- pi_load/js/vue_view.js
+++ pi_load/js/vue_view.js
@@ -178,7 +178,7 @@
         computed:{
             targetNumber(){
                 const min = 1;
-                const max = 9;
+                const max = 10;
                 return Math.floor(Math.random() * (max - min +1) + min);
             },
             viewPosition(){
